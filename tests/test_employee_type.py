3c85d5b5de8c7fed74905b2c5fb2098fc9b2a9aa
Martin Plessy <plessy.martin@outlook.com>
2021-12-20 14:41:14 +0000

    Refactored testing utility function

--- tests/test_employee_type.py
+++ tests/test_employee_type.py
@@ -1,18 +1,7 @@
 from flask.testing import FlaskClient
 from pytest import mark
-from typing import Any, Dict, List, Union
-from werkzeug import test
-
-def assert_json(response: test.TestResponse, expected_code: int, expected_body: Union[List, Dict[str, Any], None] = None):
-    assert response.status_code == expected_code
-
-    if expected_code == 204:
-        assert response.data == b''
-    elif expected_code == 404:
-        pass
-    else:
-        assert response.mimetype == 'application/json'
-        assert response.json == expected_body
+from typing import Any, Dict
+from utils import assert_json
 
 # GET /employee-type
 # -----------------------------------------------------------------------------
